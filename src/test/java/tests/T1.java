024ebf55b545463a6f3e8d4e914856c6ebf82526
itsix <cosmin.oance@itsix.com>
2017-02-14 12:23:14 +0200

    start

---
+++ src/test/java/tests/T1.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class T1  {
+
+  @Test
+  public void doNothing(){
+    System.out.println("cosmin");
+  }
+  @Test
+  public void doNothing2(){
+    
+  }
+  @Test
+  public void doNothing3(){
+    
+  }
+}
